a40ab494028d074edb4523667bc64b065e1f6da1
Martin Holman <martin308@gmail.com>
2018-09-17 12:54:43 -0700

    We don't need to modify the path

--- features/scripts/create_unity_project.sh
+++ features/scripts/create_unity_project.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 
-export PATH="$PATH:/Applications/Unity/Unity.app/Contents/MacOS"
+alias unity="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
 
 pushd "${0%/*}"
   pushd ../..
@@ -11,20 +11,20 @@ pushd "${0%/*}"
     log_file="$package_path/unity.log"
     project_path="$(pwd)/unity_project"
 
-    Unity -nographics -quit -batchmode -logFile $log_file \
+    unity -nographics -quit -batchmode -logFile $log_file \
       -createProject $project_path
 
-    Unity -nographics -quit -batchmode -logFile $log_file \
+    unity -nographics -quit -batchmode -logFile $log_file \
       -projectPath $project_path \
       -importPackage "$package_path/Bugsnag.unitypackage"
 
     cp Main.cs unity_project/Assets/Main.cs
 
-    Unity -nographics -quit -batchmode -logFile $log_file \
+    unity -nographics -quit -batchmode -logFile $log_file \
       -projectPath $project_path \
       -executeMethod "Main.CreateScene"
 
-    Unity -nographics -quit -batchmode -logFile $log_file \
+    unity -nographics -quit -batchmode -logFile $log_file \
       -projectPath $project_path \
       -buildOSXUniversalPlayer "$(pwd)/Mazerunner.app"
   popd
